6b19d47c7741e5476bdfdb6829ae4142682731dd
Mergim <mergimberisha0@gmail.com>
2023-05-23 10:48:17 +0100

    finish pairing for the day with Christie

---
+++ fizzbuzz.js
@@ -0,0 +1,11 @@
+const fizzBuzz = (number) => {
+    if (number % 5 === 0 && number % 3 === 0) {
+      console.log("FizzBuzz");
+    } else if (number % 3 === 0) {
+      console.log("Fizz");
+    } else if (number % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(number);
+    }
+  };
