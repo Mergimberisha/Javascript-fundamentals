ab5574a15a3cc1ab4a5ad88444fe1ec5d41a8e6f
Mergim <mergimberisha0@gmail.com>
2023-05-26 10:00:12 +0100

    complete debugging challenge with christie

--- phase2/basket/shopping_basket.js
+++ phase2/basket/shopping_basket.js
@@ -1,7 +1,13 @@
 class ShoppingBasket {
     constructor() {
         this.basket = [];
+        this.discount = 0;
     }
+
+    applyDiscount(discount) {
+        this.discount = discount;
+      }
+
     addItem(candy) {
         this.basket.push(candy)
     }
@@ -10,8 +16,17 @@ class ShoppingBasket {
         this.basket.forEach((candy) => {
           total += candy.cost
         });
-        return total;
+        return total - this.discount;
     }
 };
 
+const Candy = require("./candy");
+
 module.exports = ShoppingBasket;
+
+const sweet = new Candy("Mars", 10.00);
+
+const basket = new ShoppingBasket;
+basket.applyDiscount(1);
+basket.addItem(sweet);
+console.log(basket.getTotalPrice());
