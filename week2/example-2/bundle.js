936fa4c8947f9486d5533d5198c8c271b9d534d3
Mergim <mergimberisha0@gmail.com>
2023-05-30 16:02:35 +0100

    complete example-2

--- week2/example-2/bundle.js
+++ week2/example-2/bundle.js
@@ -11,6 +11,11 @@
           this.mainContainerEl = document.querySelector("#main-container");
           console.log(this.mainContainerEl);
         }
+        addParagraph() {
+          let p = document.createElement("p");
+          p.textContent = "This paragraph has been dynamically added by JavaScript!";
+          this.mainContainerEl.append(p);
+        }
       };
       module.exports = View2;
     }
@@ -19,4 +24,5 @@
   // index.js
   var View = require_view();
   var view = new View();
+  view.addParagraph();
 })();
